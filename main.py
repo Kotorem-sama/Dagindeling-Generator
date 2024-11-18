6cd66168b1e821749ebda9790da210d414eef36a
Rick Slingerland <74539046+Kotorem-sama@users.noreply.github.com>
2024-11-18 21:31:18 +0100

    Alle test ingewerkte locaties overgezet naar csv

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from classes.read_files import json_file
+from classes.read_files import csv_file
 from classes.werknemers import Werknemers
 from classes.locaties import locaties
 from random import *
@@ -19,10 +19,20 @@ def randomise_list(given_list:list, total:int):
     return random_list
 
 werknemers = Werknemers('data/werknemers.json')
-werknemers.save_to_file()
+# werknemers.save_to_file()
 
 locatie_list = locaties('data/locaties.json')
-locatie_list.save_to_file()
+# locatie_list.save_to_file()
 
 aanwezigen = Werknemers()
 aanwezigen.to_class(randomise_list(werknemers.medewerkers[:], 35))
+
+list = [[ "Personeelsnummer", "Naam" ]]
+for i in locatie_list.locaties:
+    list[0].append(f"{i.naam} ({i.id})")
+
+for i in werknemers.medewerkers:
+    list.append([i.personeelsnummer, i.naam])
+    list[-1].extend(["x" if j in i.ingewerkte_locaties else "" for j in range(1,33)])
+
+csv_file.write('data/ingewerkte_locaties.csv', list)
